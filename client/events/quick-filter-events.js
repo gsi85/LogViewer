7f60bc47ccdd392e4507ebc4a1c41c09941956aa
Laszlo Sisa <sisa.laszlo@gmail.com>
2016-02-17 18:19:50 +0100

    refactor pt1

--- client/events/quick-filter-events.js
+++ client/events/quick-filter-events.js
@@ -1,12 +1,30 @@
-var quickFilters = {};
-
 Template.quickFilter.events({
     "click #all-filter-button": function(event) {
-        setAllSelectedState(event.target);
-        Session.set("quickFilters", quickFilters);
+        quickFilterEventModule.allFilterButtonPressed(event.target);
     },
     "click .filter-button": function(event) {
-        var button = event.target;
+        quickFilterEventModule.filterButtonPressed(event.target);
+    }
+});
+
+var quickFilterEventModule = (function() {
+    var quickFilterEvent = {};
+    
+    quickFilters = {};
+
+    ALL_FILTER_BUTTON_ID = "#all-filter-button";
+    SUBMIT_BUTTON_PUSHED_CLASS = ".submit-button-pushed";
+    SUBMIT_BUTTON_CLASS = ".submit-button";
+    FILTER_BUTTON_CLASS = ".filter-button";
+    SUBMIT_BUTTON_PUSHED = "submit-button-pushed";
+    SUBMIT_BUTTON = "submit-button";
+
+    quickFilterEvent.allFilterButtonPressed = function(button) {
+        setAllSelectedState(button);
+        Session.set("quickFilters", this.quickFilters);
+    };
+
+    quickFilterEvent.filterButtonPressed = function(button) {
         togleButtonStyle(button);
         if (isAllSelectedStateEligable(button)) {
             setAllSelectedState(button);
@@ -14,56 +32,59 @@ Template.quickFilter.events({
             removeAllSelectedState(button);
         }
         Session.set("quickFilters", quickFilters);
-    }
-});
+    };
 
-var isAllSelectedStateEligable = function(button) {
-    var filterContainer = $(button).parent();
-    return filterContainer.find(".submit-button-pushed").not("#all-filter-button").length == 0 || filterContainer.find(".submit-button").not("#all-filter-button").length == 0;
-}
+    function isAllSelectedStateEligable(button) {
+        var filterContainer = $(button).parent();
+        return filterContainer.find(SUBMIT_BUTTON_PUSHED_CLASS).not().length == 0 || filterContainer.find(SUBMIT_BUTTON_CLASS).not(ALL_FILTER_BUTTON_ID).length == 0;
+    };
 
-var setAllSelectedState = function(button) {
-    $(button).parent().find(".filter-button").each(function(index) {
-        $(this).removeClass("submit-button-pushed");
-        if (!$(this).hasClass("submit-button")) {
-            $(this).addClass("submit-button");
+    function setAllSelectedState(button) {
+        $(button).parent().find(FILTER_BUTTON_CLASS).each(function(index) {
+            $(this).removeClass(SUBMIT_BUTTON_PUSHED);
+            if (!$(this).hasClass(SUBMIT_BUTTON)) {
+                $(this).addClass(SUBMIT_BUTTON);
+            }
+        });
+        if (!$(button).parent().find(ALL_FILTER_BUTTON_ID).hasClass(SUBMIT_BUTTON_PUSHED)) {
+            $(button).parent().find(ALL_FILTER_BUTTON_ID).addClass(SUBMIT_BUTTON_PUSHED);
         }
-    });
-    if (!$(button).parent().find("#all-filter-button").hasClass("submit-button-pushed")) {
-        $(button).parent().find("#all-filter-button").addClass("submit-button-pushed");
+        $(button).parent().find(ALL_FILTER_BUTTON_ID).removeClass(SUBMIT_BUTTON);
+        allSelectedQuickFilterState(button);
+    };
+
+    function togleButtonStyle(button) {
+        $(button).toggleClass(SUBMIT_BUTTON_PUSHED);
+        $(button).toggleClass(SUBMIT_BUTTON);
+        alterQuickFilterState(button);
+    };
+
+    function removeAllSelectedState(button) {
+        if (!$(button).parent().find(ALL_FILTER_BUTTON_ID).hasClass(SUBMIT_BUTTON)) {
+            $(button).parent().find(ALL_FILTER_BUTTON_ID).addClass(SUBMIT_BUTTON);
+        }
+        $(button).parent().find(ALL_FILTER_BUTTON_ID).removeClass(SUBMIT_BUTTON_PUSHED);
     }
-    $(button).parent().find("#all-filter-button").removeClass("submit-button");
-    allSelectedQuickFilterState(button);
-}
 
-var togleButtonStyle = function(button) {
-    $(button).toggleClass("submit-button-pushed");
-    $(button).toggleClass("submit-button");
-    alterQuickFilterState(button);
-}
-
-var removeAllSelectedState = function(button) {
-    if (!$(button).parent().find("#all-filter-button").hasClass("submit-button")) {
-        $(button).parent().find("#all-filter-button").addClass("submit-button");
-    }
-    $(button).parent().find("#all-filter-button").removeClass("submit-button-pushed");
-}
-
-var alterQuickFilterState = function(button) {
-    var filterCategory = $(button).parent().find("span:first-child").text();
-    var filteredValue = $(button).text();
-    if ($(button).hasClass("submit-button-pushed")) {
-        if (quickFilters[filterCategory]) {
-            quickFilters[filterCategory].push(filteredValue);
+    function alterQuickFilterState(button) {
+        var filterCategory = $(button).parent().find("span:first-child").text();
+        var filteredValue = $(button).text();
+        if ($(button).hasClass(SUBMIT_BUTTON_PUSHED)) {
+            if (quickFilters[filterCategory]) {
+                quickFilters[filterCategory].push(filteredValue);
+            } else {
+                quickFilters[filterCategory] = [filteredValue];
+            }
         } else {
-            quickFilters[filterCategory] = [filteredValue];
+            quickFilters[filterCategory].splice(quickFilters[filterCategory].indexOf(filteredValue), 1);
         }
-    } else {
-        quickFilters[filterCategory].splice(quickFilters[filterCategory].indexOf(filteredValue), 1);
     }
-}
 
-var allSelectedQuickFilterState = function(button) {
-    var filterCategory = $(button).parent().find("span:first-child").text();
-    delete quickFilters[filterCategory];
-}
+    function allSelectedQuickFilterState(button) {
+        var filterCategory = $(button).parent().find("span:first-child").text();
+        delete quickFilters[filterCategory];
+    }
+
+    return quickFilterEvent;
+    
+}());
--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -1,4 +1,5 @@
 Template.registerHelper('logs', function() {
+
     if (Session.get("quickFilters") && Object.keys(Session.get("quickFilters")).length > 0) {
         return Log.find({
             $and: [{
@@ -24,25 +25,27 @@ Template.registerHelper('logs', function() {
                 "headers.timeStamp": -1
             }
         });
-    }
+    };
+
+
+    function buildQuickFilterSelector() {
+        var quickFilters = Session.get("quickFilters");
+        var filterConditions = [];
+        for (var category in quickFilters) {
+            quickFilters[category].forEach(function(filterValue) {
+                var filterCondition = {};
+                filterCondition["headers." + category] = filterValue;
+                filterConditions.push(filterCondition);
+            });
+        }
+        return filterConditions;
+    };
+
+    function getSearchRegex() {
+        return Session.get('searchRegex') ? Session.get('searchRegex') : "";
+    };
+
 });
-
-var buildQuickFilterSelector = function() {
-    var quickFilters = Session.get("quickFilters");
-    var filterConditions = [];
-    for (var category in quickFilters) {
-        quickFilters[category].forEach(function(filterValue) {
-            var filterCondition = {};
-            filterCondition["headers." + category] = filterValue;
-            filterConditions.push(filterCondition);
-        });
-    }
-    return filterConditions;
-}
-
-var getSearchRegex = function() {
-    return Session.get('searchRegex') ? Session.get('searchRegex') : "";
-}
 
 Template.registerHelper('watchedFiles', function() {
     return WatchedFile.find({});
